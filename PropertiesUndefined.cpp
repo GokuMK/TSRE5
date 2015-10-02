68286712b7211312e1d8ca60053b54138e2652a0
GokuMK <gad03@interia.pl>
2015-10-03 01:07:25 +0200

    properties

---
+++ PropertiesUndefined.cpp
@@ -0,0 +1,32 @@
+
+#include "PropertiesUndefined.h"
+#include "WorldObj.h"
+
+PropertiesUndefined::PropertiesUndefined(){
+    
+    QVBoxLayout *vbox = new QVBoxLayout;
+    vbox->setSpacing(2);
+    vbox->setContentsMargins(0,1,1,1);
+    infoLabel = new QLabel("Select to see properties.");
+    infoLabel->setStyleSheet("QLabel { color : #999999; }");
+    infoLabel->setContentsMargins(3,0,0,0);
+    vbox->addWidget(infoLabel); 
+    
+    
+    vbox->addStretch(1);
+    this->setLayout(vbox);
+}
+
+PropertiesUndefined::~PropertiesUndefined() {
+}
+
+void PropertiesUndefined::showObj(WorldObj* obj){
+    if(obj == NULL)
+        infoLabel->setText("Select to see properties.");
+    else
+        infoLabel->setText("Unsupported: "+obj->type);
+}
+
+bool PropertiesUndefined::support(WorldObj* obj){
+    return true;
+}
